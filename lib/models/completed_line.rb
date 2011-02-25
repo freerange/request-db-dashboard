16ffe2710d97d90dbe4b2da5a4981462a54c595e
Kalvir Sandhu <kalv@kalv.co.uk>
2011-02-25 12:56:27 +0000

    Determine average response rate

--- lib/models/completed_line.rb
+++ lib/models/completed_line.rb
@@ -1,3 +1,6 @@
 class CompletedLine < ActiveRecord::Base
   
+  def self.average_response_rate
+    (self.average("duration") * 1000).round.to_f / 1000
+  end
 end
--- test/unit/completed_line_test.rb
+++ test/unit/completed_line_test.rb
@@ -7,4 +7,7 @@ class CompletedLineTest < Test::Unit::TestCase
     assert_equal 4004, CompletedLine.count
   end
 
+  def test_be_able_to_work_out_average_response_rate
+    assert_equal 0.114, CompletedLine.average_response_rate
+  end
 end
