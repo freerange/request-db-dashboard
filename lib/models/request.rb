004fa0af0f657dca389dda78d6252cece23597fa
Kalvir Sandhu <kalv@kalv.co.uk>
2011-02-28 11:12:00 +0000

    Work out requests that take longer than 1 second

--- lib/models/request.rb
+++ lib/models/request.rb
@@ -6,4 +6,15 @@ class Request < ActiveRecord::Base
     res = select("AVG(completed_lines.duration) as average_request_time").joins(:completed_line).first.average_request_time
     (res * 1000).round.to_f / 1000
   end
+
+  def self.slow
+    requests = joins([:processing_line, :completed_line]).where("completed_lines.duration > 1").group("").all
+    requests.inject({}) { |h,request|
+      controller_action = "#{request.processing_line.controller}##{request.processing_line.action}.#{request.processing_line.format}"
+      h[controller_action] ||= {:times => 0, :durations => []}
+      h[controller_action][:times]+=1
+      h[controller_action][:durations] << request.completed_line.duration
+      h
+    }.sort { |a,b| b[1][:times] <=> a[1][:times] }
+  end
 end
--- test/unit/request_test.rb
+++ test/unit/request_test.rb
@@ -10,4 +10,14 @@ class RequestTest < Test::Unit::TestCase
     assert_equal 0.114, Request.average_response_rate
   end
 
+  def test_find_slow_requests
+    slow_requests = Request.slow
+    assert_equal 11, slow_requests.size
+    assert_equal "Account::SmsConfirmationsController#update.HTML", slow_requests[0][0], "Ordered by most frequently called"
+    assert_equal 9, slow_requests[0][1][:times]
+
+    assert_equal "ConversationsController#index.HTML", slow_requests[1][0], "Ordered by most frequently called"
+    assert_equal 8, slow_requests[1][1][:times]
+  end
+
 end
