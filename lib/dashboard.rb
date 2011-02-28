81ab0c87ffe4454cfbeeb40b2890dcf945be1a20
Kalvir Sandhu <kalv@kalv.co.uk>
2011-02-28 12:53:48 +0000

    Add some design

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -13,7 +13,7 @@ class Dashboard < Sinatra::Base
   end
 
   configure do
-    set :root, File.expand_path("../", __FILE__)
+    set :root, File.expand_path("../..", __FILE__)
   end
 
   get "/" do
