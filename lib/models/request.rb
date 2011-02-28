39994a2614109b94f71d94e5aaa5e10de408c795
Kalvir Sandhu <kalv@kalv.co.uk>
2011-02-28 10:29:43 +0000

    Neater to use new finder methods

--- lib/models/request.rb
+++ lib/models/request.rb
@@ -3,7 +3,7 @@ class Request < ActiveRecord::Base
   has_one :processing_line
 
   def self.average_response_rate
-    res = find(:first, :joins => :completed_line, :select => "AVG(completed_lines.duration) as average_request_time").average_request_time
+    res = select("AVG(completed_lines.duration) as average_request_time").joins(:completed_line).first.average_request_time
     (res * 1000).round.to_f / 1000
   end
 end
